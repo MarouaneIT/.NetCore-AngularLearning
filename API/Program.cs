da04799e424f5c106b43028c21b12e4ecd8e8768
Marouane AGOUNAT <marouane.agounat@dxc.com>
2023-03-11 20:23:33 +0100

    upgrade .net core 3.1 to .net core 6

--- API/Program.cs
+++ API/Program.cs
@@ -2,25 +2,113 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using API;
+using API.Errors;
+using API.Middleware;
+using Core.Interfaces;
+using Infrastructure.Data;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using StackExchange.Redis;
 
-namespace API
+var builder = WebApplication.CreateBuilder(args);
+
+//add builder.Services to container
+
+builder.Services.AddControllers();
+builder.Services.AddDbContext<StoreContext>(x =>
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
+    x.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
-    }
+builder.Services.AddSingleton<IConnectionMultiplexer>(c =>
+{
+    var options = ConfigurationOptions.Parse(builder.Configuration.GetConnectionString("Redis"));
+    return ConnectionMultiplexer.Connect(options);
+});
+
+builder.Services.AddScoped<IBasketRepositoty, BasketRepositoty>();
+builder.Services.AddScoped<IProductRepository,ProductRepository>();
+builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+builder.Services.AddSwaggerGen();
+builder.Services.Configure<ApiBehaviorOptions>(options =>
+{
+    options.InvalidModelStateResponseFactory = actionContext =>
+    {
+        var errors = actionContext.ModelState
+        .Where(x => x.Value.Errors.Count > 0)
+        .SelectMany(x => x.Value.Errors)
+        .Select(x => x.ErrorMessage).ToArray();
+
+        var errorResponse = new ApiValidationErrorResponse
+        {
+            Errors = errors
+        };
+        return new BadRequestObjectResult(errorResponse);
+    };
+});
+
+builder.Services.AddCors(opt =>
+{
+    opt.AddPolicy("CorsPolicy", policy =>
+    {
+        policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200");
+    });
+});
+
+
+//configure the http request pipeline
+
+var app = builder.Build();
+
+app.UseMiddleware<ExeptionMiddleware>();
+
+app.UseStatusCodePagesWithReExecute("/errors/{0}");
+
+if (builder.Environment.IsDevelopment())
+{
+
+    app.UseSwagger();
+    app.UseSwaggerUI();
+    // app.UseDeveloperExceptionPage();
 }
+
+app.UseHttpsRedirection();
+
+app.UseCors("CorsPolicy");
+
+app.UseAuthorization();
+
+app.UseStaticFiles();
+
+
+app.MapControllers();
+
+
+using var scope = app.Services.CreateScope();
+var services = scope.ServiceProvider;
+var loggerFactory = services.GetRequiredService<ILoggerFactory>();
+try
+{
+
+    var context = services.GetRequiredService<StoreContext>();
+    await context.Database.MigrateAsync();
+    await SeedDataContext.SeedAsync(context, loggerFactory);
+
+}
+catch (Exception ex)
+{
+    var logger = loggerFactory.CreateLogger<Program>();
+    logger.LogError(ex, "Error logged during migration");
+}
+
+
+await app.RunAsync();
+
